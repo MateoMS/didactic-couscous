1b5b66cf53ac82ffcb01fd69785dad94daca7fd5
MateoMS <mateomolinas@gmail.com>
2021-03-18 14:31:26 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { infoPagina } from '../interfaces/info-pagina.interface';
+import { infoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +10,40 @@ export class InfoPaginaService {
 
   info: infoPagina = {};
   cargada = false;
+  equipo: infoEquipo[] = [];
 
   constructor( private http: HttpClient ) { 
 
    console.log("service");
-
-   this.http.get('assets/data/data-pagina.json')
-            .subscribe( (resp: infoPagina) => {
-
-              this.cargada = true;
-              this.info = resp;
-              console.log(resp);
-
-            });
+   this.cargarInfo();
+   this.cargarEquipo();
+   
+  }
+  
+  private cargarInfo() {
+    
+    this.http.get('assets/data/data-pagina.json')
+             .subscribe( (resp: infoPagina) => {
+  
+               this.cargada = true;
+               this.info = resp;
+               // console.log(resp);
+  
+             });
 
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-ba4e0-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: infoEquipo[]) => {
+
+      this.cargada = true;
+      this.equipo = resp;
+      
+      // console.log(resp);
+      // console.log(this.equipo);
+
+    });
+  }
+
 }
